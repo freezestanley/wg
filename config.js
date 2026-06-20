5437b9124e49189830daf023c8740a42c5b51295
za-stanleyxu <xuxiang@zhongan.io>
2026-06-20 11:56:23 +0800

    session_watch

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 module.exports = {
   "paths": {
-    "projectsRoot": "/var/folders/vm/h654vq2d2vq_8rhmncxb_63c0000gn/T/webgen-projects-init-sQG0UF"
+    "projectsRoot": "～/classroom/projects",
+  },
+  "publish": {
+    "enabled": true
   }
 };
